7bf1355474fa3f02bd0b9f0a2771c1a5d2af9062
ash <113773607+Aishaamalik@users.noreply.github.com>
2024-10-23 14:05:44 +0500

    the dependencies added

--- App.jsx
+++ App.jsx
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import ConnectyCube from 'react-native-connectycube';
+
+const CREDENTIALS = {
+  appId: 8011, 
+  authKey: 'U-Htqg5RVjK-j3S', 
+  authSecret: 'Uqtb4xpWjuTUbwv', 
+};
 
 const App = () => {
+  useEffect(() => {
+    ConnectyCube.init(CREDENTIALS);
+  }, []);
+
   return (
     <View>
-      <Text>App</Text>
+      <Text>Welcome to ConnectyCube App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
